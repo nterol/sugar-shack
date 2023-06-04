e403d3b5ae614d60170576b27f5678aaebd30489
nterol <nicolas@once.app>
2023-06-04 12:08:16 +0200

    🗃️ add images to seed file

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,14 @@ await import("./src/env.mjs");
 
 const config = {
   reactStrictMode: true,
-
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "openailabsprodscus.blob.core.windows.net",
+      },
+    ],
+  },
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
